2f2c65eb0322016dacf772945c5dbb93c3d125ac
uronohan <58647427+uronohan@users.noreply.github.com>
2020-02-17 13:21:41 +0700

    Myday 1 of JS

---
+++ index.js
@@ -0,0 +1,30 @@
+// const it = "go";
+// it = "Hello";
+// console.log(it);
+
+// let human;
+// let name; 
+// name = "Isa";
+// human = name
+// console.log(human);
+
+// let myWallet, myParentsName, myAddress,ageOfUniverse
+
+
+
+// let name = "CodeCampe";
+// console.log(`Hello ${1}`);
+// console.log(`hello ${"name"}`);
+// console.log(`hello ${name}`);
+
+// let name = "Isa";
+// age = "25";
+// address = "Bangkok, Thailand";
+// history = `my name is ${name}, I am ${age} years old and I am from ${address}`;
+// console.log(history);
+
+let name = "Isa";
+age = "26";
+address ="Bangkok, Thailand";
+console.log(`This is my history: my name isa ${name}, I am ${age} years old and I am from ${address}`);
+
