df60af13af07c93772b011f10d005c818cd1fdcd
Isa Phuyuthanon <58647427+uronohan@users.noreply.github.com>
2020-02-24 16:45:16 +0700

    JavaScript Day 4

---
+++ main1.js
@@ -0,0 +1,13 @@
+//object
+
+let user = {
+    name: 'John',
+    'Computer skill': 'JavaScript',
+}
+user.height = 170;
+user.isSingle = true;
+user.bootCamp = false;
+console.log(user);
+console.log(user.height);
+console.log(user.isSingle);
+console.log(user.bootCamp);
---
+++ main2.js
@@ -0,0 +1,55 @@
+// let human = {};
+// human.name = "Isa",
+// human.age = "25",
+// human.group = "Pee Nat",
+// human.isSingle = true,
+// human['Intelligent Score'] = 9.99,
+// console.log(human);
+
+// let enterValue = prompt("Please enter your key:");
+// let name = prompt('Name');
+
+// let obj = {}
+
+// obj[enterValue] = Name;
+
+// console.log(obj)
+
+// let obj = {
+// }
+
+// let v1 = 'firstName';
+
+
+// obj[v1] = "Isa";
+// obj["lastName"] = "Phuyuthanon";
+// obj["height"] = 170;
+// obj["age"] = 25;
+
+// console.log(obj)
+
+let obj = {};
+
+while (9==9) {
+    let fruitName = prompt('Please enter your fruit');
+ if (fruitName == 'stop') break;
+    let fruitNumber = prompt('fruitNumber');
+if (fruitNumber > 1) {
+    fruitName = fruitName + 's'
+}
+
+
+obj[fruitName] = fruitNumber;
+
+}
+
+console.log(obj)
+
+
+
+
+// let obj = {}
+
+// obj[fruitMultiple] = Name + "s";
+
+// console.log(obj)
