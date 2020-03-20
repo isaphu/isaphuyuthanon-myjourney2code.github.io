a90739a72051455fe5c0d02c9798d4dbc746f9c8
Isa Phuyuthanon <58647427+isaphu@users.noreply.github.com>
2020-03-20 17:06:46 +0700

    react day 2 prototype

---
+++ reactDay2.5.js
@@ -0,0 +1,15 @@
+// function f() {
+//     alert("Hello!");
+// }
+
+// f.defer(1000);
+
+Function.prototype.defer = function (ms) {
+    setTimeout(this, ms);
+};
+
+function f() {
+    alert("Hello!");
+}
+
+f.defer(1000); 
